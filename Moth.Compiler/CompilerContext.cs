10a07ddac1da3d74b47f68c9a44f8e695602c45a
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2023-07-24 12:38:48 -0400

    Blep

--- Moth.Compiler/CompilerContext.cs
+++ Moth.Compiler/CompilerContext.cs
@@ -12,8 +12,6 @@ public class CompilerContext
     public LLVMContextRef Context { get; set; }
     public LLVMBuilderRef Builder { get; set; }
     public LLVMModuleRef Module { get; set; }
-    public Dictionary<string, LLVMTypeRef> Types = new Dictionary<string, LLVMTypeRef>();
-    public Dictionary<string, LLVMValueRef> Functions = new Dictionary<string, LLVMValueRef>();
 
     public CompilerContext(string moduleName)
     {
